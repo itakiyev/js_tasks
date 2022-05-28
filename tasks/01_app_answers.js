e79beba7fc13ecb3ac42c1c374060baf8169a556
Issa Takiyev <issa.takiyev@gmail.com>
2022-05-28 16:48:20 +0600

    updated

--- tasks/01_app_answers.js
+++ tasks/01_app_answers.js
@@ -30,3 +30,16 @@ function findSum() {
 
     return sum;
 }
+
+// #3
+
+function roundToNext5(n) {
+    return Math.ceil(n / 5) * 5;
+}
+
+function roundToNext5(n) {
+    while (n % 5 !== 0) n++;
+    return n;
+}
+
+const roundToNext5 = n => Math.ceil(n / 5) * 5;
